3e69a56021a3fd1951fdc00372d6f39b8b3ccd62
hoc081098@gmail.com <hoc081098@gmail.com>
2024-08-17 15:48:38 +0700

    wip

--- app/src/main/java/com/hoc081098/stickybottomsheet/two_composeviews/TwoComposeViewsStickyBottomSheet.kt
+++ app/src/main/java/com/hoc081098/stickybottomsheet/two_composeviews/TwoComposeViewsStickyBottomSheet.kt
@@ -4,11 +4,10 @@ import android.app.Dialog
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -21,15 +20,17 @@ import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
 import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
 import androidx.compose.ui.unit.dp
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.core.view.updateLayoutParams
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.PEEK_HEIGHT_AUTO
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.hoc081098.stickybottomsheet.R
-import com.hoc081098.stickybottomsheet.bottomSheetDialogDefaultHeight
 import com.hoc081098.stickybottomsheet.bottomSheetSlides
 import com.hoc081098.stickybottomsheet.databinding.FragmentTwoComposeViewsStickyBottomSheetBinding
 import com.hoc081098.stickybottomsheet.requireBottomSheetBehavior
@@ -38,9 +39,14 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 
@@ -49,8 +55,21 @@ class TwoComposeViewsStickyBottomSheet :
     private val binding by viewBinding<FragmentTwoComposeViewsStickyBottomSheetBinding>()
 
     private val buttonHeightFlow = MutableStateFlow(0)
-    private var collapsedMargin = 0
-    private var expandedHeight = 0
+    private val expandedHeightFlow = MutableStateFlow(0)
+    private val peekHeightFlow = expandedHeightFlow
+        .map { it * 0.7f }
+        .filter { it > 0 }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return (super.onCreateDialog(savedInstanceState) as BottomSheetDialog).apply {
+            behavior.run {
+                state = BottomSheetBehavior.STATE_COLLAPSED
+                peekHeight = PEEK_HEIGHT_AUTO
+                skipCollapsed = false
+                isHideable = true
+            }
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -62,7 +81,9 @@ class TwoComposeViewsStickyBottomSheet :
             val nestedScrollConnection = rememberNestedScrollInteropConnection()
             MaterialTheme {
                 Surface(
-                    modifier = Modifier.wrapContentHeight(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .onSizeChanged { expandedHeightFlow.value = it.height },
                     color = MaterialTheme.colorScheme.background
                 ) {
                     LazyColumn(
@@ -82,15 +103,18 @@ class TwoComposeViewsStickyBottomSheet :
             }
         }
 
-        binding.sheetButtonComposeView
-            .setViewCompositionStrategy(DisposeOnViewTreeLifecycleDestroyed)
+        binding.sheetButtonComposeView.setViewCompositionStrategy(
+            DisposeOnViewTreeLifecycleDestroyed
+        )
         binding.sheetButtonComposeView.setContent {
             MaterialTheme {
                 Button(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .onSizeChanged { buttonHeightFlow.value = it.height },
+                        .onSizeChanged { buttonHeightFlow.value = it.height }
+                        .padding(all = 24.dp),
                     onClick = { dismiss() },
+                    contentPadding = PaddingValues(horizontal = 24.dp, vertical = 20.dp),
                 ) {
                     Text("Dismiss")
                 }
@@ -98,28 +122,41 @@ class TwoComposeViewsStickyBottomSheet :
         }
 
         observeBottomSheetSlides()
+        observePeekHeightFlow()
+    }
+
+    private fun observePeekHeightFlow() {
+        val bottomSheetBehavior = requireBottomSheetBehavior()
+        peekHeightFlow
+            .onEach { bottomSheetBehavior.setPeekHeight(it.toInt(), true) }
+            .launchIn(lifecycleScope)
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     private fun observeBottomSheetSlides() {
         val bottomSheetBehavior = requireBottomSheetBehavior()
 
-        val topMarginFlow = buttonHeightFlow
-            .flatMapLatest { buttonHeight ->
+        val topMarginFlow = combine(buttonHeightFlow, expandedHeightFlow, peekHeightFlow, ::Triple)
+            .distinctUntilChanged()
+            .flatMapLatest { (buttonHeight, expandedHeight, peekHeight) ->
+                if (buttonHeight <= 0 || expandedHeight <= 0 || peekHeight <= 0f) {
+                    return@flatMapLatest flowOf(-1)
+                }
+
+                // Button margin in bottom sheet collapsed state
+                val collapsedMargin = peekHeight - buttonHeight
+
                 bottomSheetBehavior
                     .bottomSheetSlides()
                     .buffer(Channel.UNLIMITED)
                     .onStart { emit(bottomSheetBehavior.calculateSlideOffset()) }
                     .map { slideOffset ->
-                        check(collapsedMargin > 0) { "collapsedMargin must be greater than 0" }
-                        check(expandedHeight > 0) { "collapsedMargin must be greater than 0" }
-
                         if (slideOffset > 0) {
                             // Sliding happens from 0 (Collapsed) to 1 (Expanded) - if so, calculate margins
                             (((expandedHeight - buttonHeight) - collapsedMargin) * slideOffset + collapsedMargin).toInt()
                         } else {
                             // If not sliding above expanded, set initial margin
-                            collapsedMargin
+                            collapsedMargin.toInt()
                         }
                     }
             }
@@ -128,49 +165,18 @@ class TwoComposeViewsStickyBottomSheet :
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 topMarginFlow.collect { newTopMargin ->
+                    if (newTopMargin > 0) {
+                        binding.sheetButtonComposeView.isVisible = true
+                    } else {
+                        binding.sheetButtonComposeView.isInvisible = true
+                    }
                     binding.sheetButtonComposeView
                         .updateLayoutParams<ViewGroup.MarginLayoutParams> {
-                            this.topMargin = newTopMargin
+                            this.topMargin = newTopMargin.coerceAtLeast(0)
                         }
                 }
             }
         }
-    }
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
-        dialog.setOnShowListener { setupRatio(dialog) }
-        return dialog
-    }
-
-    private fun setupRatio(bottomSheetDialog: BottomSheetDialog) {
-        val bottomSheet = bottomSheetDialog.findViewById<FrameLayout>(
-            com.google.android.material.R.id.design_bottom_sheet
-        ) ?: return
-
-        // Calculate expanded height and peek height
-        expandedHeight = requireActivity().bottomSheetDialogDefaultHeight
-        val peekHeight =
-            (expandedHeight * 0.7f).toInt() // Peek height to 70% of expanded height (Change based on your view)
-
-        // Setup bottom sheet
-        bottomSheet.updateLayoutParams { this.height = expandedHeight }
-        bottomSheetDialog.behavior.run {
-            state = BottomSheetBehavior.STATE_COLLAPSED
-            skipCollapsed = false
-            this.peekHeight = peekHeight
-            isHideable = true
-        }
-
-        // Calculate button margin from top
-        buttonHeightFlow.value =
-            binding.sheetButtonComposeView.height + 40 // How tall is the button + experimental distance from bottom (Change based on your view)
-        collapsedMargin =
-            peekHeight - buttonHeightFlow.value // Button margin in bottom sheet collapsed state
-
-        // Set initial top margin of button
-        binding.sheetButtonComposeView
-            .updateLayoutParams<ViewGroup.MarginLayoutParams> { this.topMargin = collapsedMargin }
     }
 
     companion object {
