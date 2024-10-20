d6b685ae31b701a40b962eadde601ccde1b37fe3
hoc081098@gmail.com <hoc081098@gmail.com>
2024-10-20 18:16:28 +0700

    demo stack

--- app/src/main/java/com/hoc081098/stickybottomsheet/MainActivity.kt
+++ app/src/main/java/com/hoc081098/stickybottomsheet/MainActivity.kt
@@ -6,7 +6,12 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.hoc081098.stickybottomsheet.databinding.ActivityMainBinding
+import com.hoc081098.stickybottomsheet.demostack.DemoStackActivity
+import com.hoc081098.stickybottomsheet.demostack.OverrideTransitionType
+import com.hoc081098.stickybottomsheet.demostack.logLifecycleEvents
+import com.hoc081098.stickybottomsheet.demostack.overridePendingTransitionCompat
 import com.hoc081098.stickybottomsheet.one_composeview.OneComposeViewStickyBottomSheet
 import com.hoc081098.stickybottomsheet.overlay.OverlayActivity
 import com.hoc081098.stickybottomsheet.overlay.OverlayPresenter
@@ -15,6 +20,8 @@ import com.hoc081098.stickybottomsheet.demoswitch.DemoSwitchActivity
 import com.hoc081098.stickybottomsheet.two_composeviews.TwoComposeViewsStickyBottomSheet
 import com.hoc081098.stickybottomsheet.viewbased.ViewBasedStickyBottomSheet
 import com.hoc081098.viewbindingdelegate.viewBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
@@ -58,8 +65,45 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         binding.buttonDemoSwitch.setOnClickListener {
             startActivity(DemoSwitchActivity.createIntent(this))
         }
+        binding.buttonDemoStack.setOnClickListener {
+            startActivity(GatewayActivity.createIntent(this))
+            finish()
+        }
 
         OverlayPresenter(this).attach(lifecycleOwner = this)
+
+        logLifecycleEvents()
+    }
+
+    companion object {
+        fun createIntent(activity: AppCompatActivity) =
+            android.content.Intent(activity, MainActivity::class.java)
     }
 }
 
+class GatewayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        overridePendingTransitionCompat(
+            OverrideTransitionType.Open,
+            0,
+            0,
+        )
+
+        lifecycleScope.launch {
+            delay(300)
+            startActivity(DemoStackActivity.createIntent(this@GatewayActivity))
+            finish()
+            overridePendingTransitionCompat(
+                OverrideTransitionType.Close,
+                0,
+                0,
+            )
+        }
+    }
+
+    companion object {
+        fun createIntent(activity: AppCompatActivity) =
+            android.content.Intent(activity, GatewayActivity::class.java)
+    }
+}
